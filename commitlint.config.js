c7ef7efe42287da996d15530721660bbd985ffc3
--global <seki4649@163.com>
2024-09-01 20:33:16 +0800

    fix: commitlint.config.js filename misspelling

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'build', 'ci', 'wip']],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'subject-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'header-min-length': [0, 'always', 72]
+  }
+};
